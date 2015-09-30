2bef67ab7e91285e2b6cc1d3e18ba72bd458bead
Kaitlyn Kwan <KKwan@Kaitlyns-MacBook-Pro.local>
2015-09-30 15:08:31 -0700

    added folders and routes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+var port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('api/getSororities', function(req, res) {
 	res.send('Hello World!');
 });
 
-var server = app.listen(3000, function() {
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('FMR app listening at http://%s:%s', host, port);
-});
