604db4d81a41ec35f5482d87bf781bbf879f20b6
Kaitlyn Kwan <KKwan@Kaitlyns-MacBook-Pro.local>
2015-10-02 10:06:07 -0700

    daf

--- app.js
+++ app.js
@@ -1,15 +1,79 @@
 var express = require('express');
+var http = require('http');
+var connect = require('connect'),
+	mongojs = require('mongojs');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var app = express();
-
-var port = process.env.PORT || 8080;
-
+var server = app.listen(process.env.PORT || 5000);
+ 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('api/getSororityNames', function(req, res) {
-	res.send('Hello World!');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+var uri = "mongodb://admin:admin@ds051863.mongolab.com:51863/heroku_bzhpj4l3";
+var db = mongojs.connect(uri, ["calendar", "info", "sororities"]);
+
+app.use(function(req,res,next) { 
+	req.db = db; 
+	next();
+}); 
+
+
+// Sorority Info
+app.get('/api/getSororityNames', function(req, res) {
+	console.log('here');
 });
 
-// getSororities
+app.get('/api/getSororities', function(req, res) {
 
+});
+
+app.post('/api/addSorority', function(req, res) {
+
+});
+
+app.get('*', function(req, res) {
+	console.log('heerererereee');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error.jade', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
